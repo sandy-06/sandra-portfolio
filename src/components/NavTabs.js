b651350622dae13fcd259262fc321f390b3c7446
Sandra Sanders <sandy-sanders@msn.com>
2022-03-10 22:54:18 -0700

    correction to navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -52,7 +52,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#resume"
           onClick={() => handlePageChange('Resume')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
         </a>
